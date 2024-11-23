f0359b318a6c0e8dd0bf427a6f84c4d6c2baf5ad
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-11-23 18:47:52 +0200

    add create patient and it's validation

---
+++ MedicalSystem.API/Contracts.cs/Patients/CreatePatientRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalSystem.API.Contracts.cs.Patients
+{
+	public record CreatePatientRequest
+	(
+		string Email,
+		string FullName,
+		string Password,
+		int Age,
+		string BloodyGroup
+	);
+}
---
+++ MedicalSystem.API/Contracts.cs/Patients/CreatePatientRequestValidation.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MedicalSystem.API.Abstractions.Consts;
+using MedicalSystem.API.Contracts.cs.Authentication;
+
+namespace MedicalSystem.API.Contracts.cs.Patients
+{
+	public class CreatePatientRequestValidation : AbstractValidator<CreatePatientRequest>
+	{
+		public CreatePatientRequestValidation()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+
+			RuleFor(x => x.FullName)
+				.NotEmpty()
+				.Length(3, 100);
+
+			RuleFor(x=>x.Age)
+				.NotEmpty()
+				.NotNull()
+				.GreaterThan(0)
+				.LessThan(150);
+
+			RuleFor(x => x.BloodyGroup)
+				.NotEmpty()
+				.NotNull();
+				//.Matches(RegexPatterns.BloodyGroup);
+
+			RuleFor(x => x.Password)
+				.NotEmpty()
+				.Matches(RegexPatterns.Password)
+				.WithMessage("Passwrod should be at least 6 digits and should contains lower case, nonalphanumeric and uppercase");
+		}
+	}
+}
