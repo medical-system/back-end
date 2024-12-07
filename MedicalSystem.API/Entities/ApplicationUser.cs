67d65e368d58617fe6b8a79f8a45bdace7e558ad
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-07 15:43:45 +0200

    add auditable entity and fix table colums issue

--- MedicalSystem.API/Entities/ApplicationUser.cs
+++ MedicalSystem.API/Entities/ApplicationUser.cs
@@ -8,13 +8,15 @@ namespace MedicalSystem.API.Entities
 		public string LastName { get; set; } = string.Empty;
 		public string FullName { get; set; } = string.Empty;
 		public string Password { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
 		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 		public string BloodyGroup { get; set; } = string.Empty;
 		public int Age { get; set; }
 		public bool IsDisabled { get; set; }
 		public string? DoctorId { get; set; }
-		public ApplicationUser Doctor { get; set; }
+		public ApplicationUser? Doctor { get; set; }
 		public List<RefreshToken> RefreshToken { get; set; } = [];
-		public ICollection<MedicalRecord> MedicalRecords { get; set; } = [];
+		public virtual ICollection<MedicalRecord> MedicalRecords { get; set; } = [];
+		public List<RefreshToken> RefreshTokens { get; set; } = [];
 	}
 }
--- MedicalSystem.API/Entities/Medicine.cs
+++ MedicalSystem.API/Entities/Medicine.cs
@@ -1,9 +1,9 @@
 ﻿namespace MedicalSystem.API.Entities
 {
-	public class Medicine
+	public class Medicine 
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string Name { get; set; } = string.Empty;
 		public decimal Price { get; set; }
 		public bool Status { get; set; }
 		public int InStock { get; set; }
