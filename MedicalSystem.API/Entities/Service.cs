d799ee3fe7bbb15c86eebd92f3b89630b7bea542
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-11 03:30:28 +0200

    add service entity

---
+++ MedicalSystem.API/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalSystem.API.Entities
+{
+	public class Service : AuditableEntity
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ MedicalSystem.API/Persistence/EntitiesConfigrations/ServicesConfigration.cs
@@ -0,0 +1,21 @@
+﻿using MedicalSystem.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MedicalSystem.API.Persistence.EntitiesConfigrations
+{
+	public class ServicesConfigration : IEntityTypeConfiguration<Service>
+	{
+		public void Configure(EntityTypeBuilder<Service> builder)
+		{
+			builder.Property(m => m.Id)
+				.ValueGeneratedOnAdd()
+				.UseIdentityColumn(1, 1);
+			builder.Property(m => m.Price)
+				.HasColumnType("decimal(18,2)");
+
+			builder.Property(p => p.Status)
+			  .HasConversion(p => p.ToString(), Statu => (Status)Enum.Parse(typeof(Status), Statu));
+		}
+	}
+}
