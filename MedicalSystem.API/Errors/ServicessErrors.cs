a848f7e59b16c3aea7981bf54bad297ed193e360
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-13 20:00:09 +0200

    add service error handling

---
+++ MedicalSystem.API/Errors/ServicessErrors.cs
@@ -0,0 +1,12 @@
+﻿using MedicalSystem.API.Abstractions;
+
+namespace MedicalSystem.API.Errors
+{
+	public static class ServicessErrors
+	{
+		public static readonly Error DuplicatedName = new("Service.DuplicatedName", "Another Service with the same Name is already exists", StatusCodes.Status409Conflict);
+
+		public static readonly Error ServiceNotFound = new("Service.ServiceNotFound", "Service is not found", StatusCodes.Status404NotFound);
+
+	}
+}
