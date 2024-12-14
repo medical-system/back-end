b06dc1d0bc94088951e54179d6151d2bda81c35a
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-14 23:47:13 +0200

    add servire response

---
+++ MedicalSystem.API/Contracts.cs/Servicess/ServicessResponse.cs
@@ -0,0 +1,13 @@
+﻿using MedicalSystem.API.Entities;
+
+namespace MedicalSystem.API.Contracts.cs.Servicess
+{
+	public record ServicessResponse
+	(
+		int Id,
+		string Name,
+		DateTime CreatedAt,
+		decimal Price,
+		Status Status
+	);
+}
