4cb2a7fd483c534a792f10c8731645bd83b608de
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-26 23:32:33 +0200

    add  reception response

---
+++ MedicalSystem.API/Contracts.cs/Receptions/ReceptionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalSystem.API.Contracts.cs.Receptions
+{
+	public record ReceptionResponse
+	(
+		string Id,
+		string Name,
+		DateTime CreatedAt,
+		string Phone,
+		string Title,
+		string Email
+	);
+}
