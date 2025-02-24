948fcee35102b1a25b6c7335f5139333b7b472dc
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-02-24 20:13:18 +0200

    Adding IServicessService

---
+++ MedicalSystem.API/Services/IServicessService.cs
@@ -0,0 +1,12 @@
+﻿using MedicalSystem.API.Abstractions;
+using MedicalSystem.API.Contracts.cs.Servicess;
+
+namespace MedicalSystem.API.Services
+{
+    public interface IServicessService
+    {
+        Task<IEnumerable<ServicessResponse>> GetServicesAsync(CancellationToken cancellationToken = default);
+        Task<Result<ServicessResponse>> GetServicessByIdAsync(int id);
+        Task<Result<ServicessResponse>> AddServicessAsync(ServicessRequest request);
+    }
+}
